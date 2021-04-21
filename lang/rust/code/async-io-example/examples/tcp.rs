f26543e4378665a8e1654f0f284c49a78b9548a7
iamazy <1448588084@qq.com>
2021-04-21 08:29:25 +0800

    add async-io-example

---
+++ lang/rust/code/async-io-example/examples/tcp.rs
@@ -0,0 +1,14 @@
+use mio::Token;
+use failure::Error;
+
+const SERVER_ACCEPT: Token = Token(0);
+const SERVER: Token = Token(1);
+const CLIENT: Token = Token(2);
+const SERVER_HELLO: &[u8] = b"PING";
+const CLIENT_HELLO: &[u8] = b"PONG";
+
+fn main() -> Result<(), Error> {
+    let addr = "127.0.0.1:13265".parse()?;
+
+    Ok(())
+}
